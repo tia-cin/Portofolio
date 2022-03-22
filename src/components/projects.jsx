e0bd9cf64ec45af3b9ea5f9303b651e4c9dcb70d
Cintia Arce <cintiaayelenarce@gmail.com>
2022-03-22 18:45:04 -0300

    Contact & Projects

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -1,8 +1,40 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom'
+
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 const Projects = () => {
     return(
-        <div></div>       
+        <div>
+            <div>
+            <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                    component="img"
+                    height="140"
+                    src='https://cdn3.iconfinder.com/data/icons/climate-iconset/40/Cloudy-256.png'
+                    alt="cloud icon"
+                />
+                <CardContent>
+                    <Typography gutterBottom variant="h5" component="div">
+                    Weather App
+                    </Typography>
+                    <Typography variant="body2" color="text.secondary">
+                    Application where you can see real time weather information about any city in the world
+                    </Typography>
+                </CardContent>
+                <CardActions>
+                    <a href='https://weather-app-tia-cin.vercel.app/' target='_blank'>
+                        <Button size="small">Ckeck it out</Button>
+                    </a>
+                </CardActions>
+            </Card>
+            </div>
+        </div>       
     )
 }
 
--- src/components/social.jsx
+++ src/components/social.jsx
@@ -2,7 +2,20 @@ import * as React from 'react';
 
 const Social = () => {
     return (
-        <div></div>
+        <div>
+            <a href='https://linkedin.com/in/cintia-arce-profile'>
+                <img src='https://cdn1.iconfinder.com/data/icons/social-media-rounded-corners/512/Rounded_Linkedin2_svg-128.png' alt='linkedin' width='50' height='50'/>
+            </a>
+            <a href='https://github.com/tia-cin'>
+                <img src='https://cdn3.iconfinder.com/data/icons/font-awesome-brands/512/github-square-256.png' alt='github' width='50' height='50'/>
+            </a>
+            <a href='mailto:cintiaayelenarce@gmail.com'>
+                <img src='https://cdn4.iconfinder.com/data/icons/glyphs/24/icons_email-256.png' alt='mail' width='50' height='50'/>
+            </a>
+            <a href='C:/Users/cinti/OneDrive/Documents/Programming/Cintia Arce CV.pdf' download>
+                <img src='https://cdn3.iconfinder.com/data/icons/resume-solid-essential/512/Experience_briefcase-256.png' alt='resume' width='50' heigth='50' />
+            </a>
+        </div>
     )
 }
 
