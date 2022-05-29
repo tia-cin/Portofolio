e38408fe65de242e954747031b7ff1ebc4bcb398
Cintia Arce <cintiaayelenarce@gmail.com>
2022-05-29 19:57:05 -0300

    new project

--- src/components/projects.jsx
+++ src/components/projects.jsx
@@ -2,14 +2,13 @@ import * as React from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
 import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
 import './styles/projects.css'
 
 const Projects = () => {
     return(
         <div className='projectsContainer'>
             <a href='https://weather-app-tia-cin.vercel.app/'>
-                <Card sx={{ maxWidth: 345, borderRadius: 10 }}>
+                <Card sx={{ width: '20em', borderRadius: 10 }}>
                     <div className='img1'>
                         <img
                             src='https://cdn3.iconfinder.com/data/icons/climate-iconset/40/Cloudy-256.png'
@@ -23,13 +22,13 @@ const Projects = () => {
                         Weather App
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
-                        Application where you can see real time weather information about any city in the world
+                        Real time weather information about any city in the world
                         </Typography>
                     </CardContent>
                 </Card>
             </a>
             <a href='https://deploy-client-kappa.vercel.app/'>
-                <Card sx={{ maxWidth: 345, borderRadius: 10 }}>
+                <Card sx={{ width: '20em', borderRadius: 10 }}>
                     <div className='img2'>
                         <svg width="80" height="80" viewBox="0 0 140 154" fill="none" xmlns="http://www.w3.org/2000/svg">
                             <rect width="140" height="140" rx="10" fill="#192A45"/>
@@ -41,7 +40,27 @@ const Projects = () => {
                         Mangaka
                         </Typography>
                         <Typography variant="body2" color="text.secondary">
-                        Application where you can see real time weather information about any city in the world
+                        E-Commerse app for manga/comics with reader
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </a>
+            <a href='https://music-app-self.vercel.app'>
+                <Card sx={{ width: '20em', borderRadius: 10 }}>
+                    <div className='img3'>
+                        <img 
+                            src='https://cdn-icons-png.flaticon.com/512/2972/2972319.png' 
+                            alt='music-app-logo'
+                            width='100'
+                            height='100'
+                        />
+                    </div>
+                    <CardContent>
+                        <Typography gutterBottom variant='h5' component='div'>
+                            Music App
+                        </Typography>
+                        <Typography variant="body2" color="text.secondary">
+                            Song player app made with Vue JS
                         </Typography>
                     </CardContent>
                 </Card>
